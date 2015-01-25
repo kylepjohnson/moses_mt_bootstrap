a60c7bc6eb945020c7a9f6d9a48be84df7dc432d
Kyle Johnson <kyle@kyle-p-johnson.com>
2015-01-25 16:24:05 -0500

    add travis build options

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,18 @@
-sudo apt-get update
-sudo apt-get -y upgrade
+#!/bin/bash
+######################
+# UPDATE AND UPGRADE #
+######################
+# if argument does not exist (as for user build), update aptitude; else do not
+if [ -z "$1" ]
+then
+  echo "update apt-get"
+  sudo apt-get update
+  sudo apt-get -y upgrade
+else
+  echo "do not update apt-get"
+fi
+
+
 sudo apt-get -y install build-essential git libboost-all-dev g++ git subversion automake libtool zlib1g-dev libboost-all-dev libbz2-dev liblzma-dev python-dev libgoogle-perftools-dev
 
 cd
